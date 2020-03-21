8c373a2a5083d35ebd98da603e8bde21ec0f3261
Alex Barry <alex.barry@gmail.com>
2020-03-21 16:07:21 -0400

    Add a deploy script
    Closes #24

---
+++ deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+HOST_DIR=/var/www/mobtime
+DEPLOY_DIR="$HOST_DIR/deploys"
+CONFIG_DIR="$HOST_DIR/config"
+CURRENT_LINK="$HOST_DIR/current"
+
+CURRENT_TIMESTAMP=`date +%s`
+
+CURRENT_DEPLOY="$DEPLOY_DIR/$CURRENT_TIMESTAMP"
+
+function ensureHostDirIsCreated() {
+  mkdir -p $CONFIG_DIR
+}
+
+function copySelfIntoDeploy() {
+  mkdir -p $CURRENT_DEPLOY
+  cp -r . "$CURRENT_DEPLOY/"
+}
+
+function setupCurrentDeploy() {
+  ln -sf $CURRENT_DEPLOY $CURRENT_LINK
+  if [ "$(ls -A $CONFIG_DIR)" ]; then
+    cp -R "$CONFIG_DIR/." $CURRENT_LINK
+  fi
+}
+
+ensureHostDirIsCreated
+copySelfIntoDeploy
+setupCurrentDeploy
+
