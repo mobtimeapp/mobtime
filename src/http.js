f24f9156819cd898e0f26cedd8fa8fa894642b32
Alex Barry <alex.barry@gmail.com>
2020-03-24 08:43:04 -0400

    Add statistics
     - Closes #29

--- src/http.js
+++ src/http.js
@@ -63,6 +63,24 @@ const HttpSub = (bus, storage, action, host = 'localhost', port = 4321, singleTi
     }
   });
 
+  router.get('/statistics', (_request, response) => {
+    const state = storage.read();
+    const timerIds = Object.keys(state);
+    const timerStatistics = timerIds.reduce((counts, id) => ({
+      mobbers: counts.mobbers + state[id].mob.length,
+      connections: counts.connections + state[id].tokens.length,
+      goals: counts.goals + state[id].goals.length,
+    }), { mobbers: 0, connections: 0, goals: 0 });
+
+    return response
+      .status(200)
+      .json({
+        timers: timerIds.length,
+        ...timerStatistics,
+      })
+      .end();
+  });
+
   router.use((request, response, next) => {
     const token = (request.headers.authorization || '')
       .replace(/^token /, '');
