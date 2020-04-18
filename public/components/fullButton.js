e533db1ccfe7b19962584bada4606c3237badd21
Alex Barry <alex.barry@gmail.com>
2020-04-18 16:36:33 -0400

    Stip down fullbutton component

--- public/components/fullButton.js
+++ public/components/fullButton.js
@@ -7,7 +7,6 @@ export const fullButton = (props, children) => h('button', {
     'py-3': true,
     'px-4': true,
     'text-base': true,
-    // 'md:text-3xl': true,
     ...(props.class || {}),
   },
 }, children);
