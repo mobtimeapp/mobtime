ca07ff1998fdbfd940057722d610b5031751986a
Alex Barry <alex.barry@gmail.com>
2020-04-23 09:10:55 -0400

    Converted initialize.sh to index.js, and added a config file for multiple deploy targets

---
+++ deploy/index.js
@@ -0,0 +1,85 @@
+const child_process = require('child_process');
+const path = require('path');
+
+const { appName, targets } = require('./config');
+
+const deployScript = path.resolve(__dirname, 'work.sh');
+
+const exec = (cmd, args = [], prefix = 'CMD') => new Promise((resolve, reject) => {
+  const command = `${cmd} ${args.join(' ')}`;
+  console.log(`Running \`${command}\`...`);
+
+  const proc = child_process.spawn(cmd, args, {
+    env: process.env,
+    stdio: ['inherit', 'pipe', 'pipe'],
+    encoding: 'utf8',
+    shell: true,
+  });
+
+  const formatMessage = (from, data) => {
+    const message = data.toString().trim();
+    if (!message) {
+      return [];
+    }
+
+    return message
+      .split(/(\r|\n)+/)
+      .map(line => line.trim())
+      .filter(line => !!line)
+      .map(line => `[${prefix}|${from}] ${line}`);
+  };
+
+  proc.stdout.on('data', (data) => {
+    formatMessage('stdout', data).forEach((line) => process.stdout.write(`${line}\n`));
+  });
+
+  proc.stderr.on('data', (data) => {
+    formatMessage('stderr', data).forEach((line) => process.stderr.write(`${line}\n`));
+  });
+
+  proc.on('close', (code) => {
+    return code === 0 ? resolve() : reject (code);
+  });
+});
+
+const main = async (env) => {
+  const target = targets[env.TARGET];
+
+  if (!target) {
+    const available = Object.keys(targets);
+    console.error(`Unable to deploy to ${process.env.TARGET}, must be one of ${available.join(', ')}.`);
+    return 1;
+  };
+
+  const remotePath = `/tmp/deploy_${appName}.sh`;
+
+  const sshTarget = `${target.ssh.user}@${target.ssh.host}`;
+
+  console.log('Initializing deploy script...');
+  await exec('scp', [
+    ...(target.rsa ? [`-i ${target.rsa}`] : []),
+    deployScript,
+    `${sshTarget}:${remotePath}`,
+  ], `scp ${sshTarget}`);
+  console.log('Initializing deploy script...done');
+
+  console.log('');
+
+  console.log(`Deploying to ${target.ssh.host}...`);
+  await exec('ssh', [
+    ...(target.rsa ? [`-i ${target.rsa}`] : []),
+    sshTarget,
+    'bash',
+    remotePath,
+  ], `ssh ${sshTarget}`);
+  console.log(`Deploying to ${target.ssh.host}...done`);
+
+  return 0;
+};
+
+main(process.env)
+  .catch((err) => {
+    console.error(err);
+    return 1;
+  })
+  .then(process.exit);
