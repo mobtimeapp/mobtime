5ec1ef2ea9d3ea1d03e5ee77e05195bab44e200a
Alex Barry <alex.barry@gmail.com>
2020-04-25 16:08:57 -0400

    Rename export from connection to database in database.js

--- src/database.js
+++ src/database.js
@@ -3,4 +3,4 @@ import configurations from '../knexfile';
 
 const environment = process.env.NODE_ENV || 'development';
 
-export const connection = knex(configurations[environment]);
+export const database = knex(configurations[environment]);
