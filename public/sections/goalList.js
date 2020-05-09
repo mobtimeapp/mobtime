6fa9e44710374cc6f2828cb282b5fc10df24451f
Alex Barry <alex.barry@gmail.com>
2020-05-09 00:39:48 -0400

    Use reorderable in goal ui

--- public/sections/goalList.js
+++ public/sections/goalList.js
@@ -2,11 +2,13 @@ import { h } from 'https://unpkg.com/hyperapp?module=1';
 
 import { section } from '/components/section.js';
 import { goal } from '/components/goal.js';
+import { reorderable } from '/components/reorderable.js';
 
 export const goalList = (props) => h(section, null, [
-  props.goals.map(({ id, text, completed }) => h(goal, {
-    id,
-    text,
-    completed,
-  })),
+  h(reorderable, {
+    dragType: 'goal',
+    items: props.goals,
+    renderItem: (item) => h(goal, item),
+    drag: props.drag,
+  }),
 ]);
