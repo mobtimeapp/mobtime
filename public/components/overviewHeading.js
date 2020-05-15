893d625ed3bd0c4a37ea2c602910e17ec0911a54
Alex Barry <alex.barry@gmail.com>
2020-05-14 21:52:36 -0400

    Extract overview heading

---
+++ public/components/overviewHeading.js
@@ -0,0 +1,25 @@
+import { h } from 'https://unpkg.com/hyperapp?module=1';
+
+export const overviewHeading = (props, children) => h('div', {
+  ...props,
+  class: {
+    ...props.class,
+    'px-1': true,
+    'mx-5': true,
+    'pt-3': true,
+    'flex': true,
+    'flex-row': true,
+    'items-end': true,
+    'justify-between': true,
+    'border-b': true,
+  },
+}, [
+  h('h2', {
+    class: {
+      'text-lg': true,
+      'font-bold': true,
+      'py-1': true,
+    },
+  }, children),
+  props.rightAction || h('div', {}),
+]);
