f43cf77fa74f2ecdcefc8c2b77e35d5c32ef22eb
Vehikl Team <nomorobo-vehikl@gmail.com>
2020-08-14 12:02:43 -0400

    Trim down server app, and start testing new actions

--- database/migrations/20200425154552_CreateBlacklistTable.js
+++
@@ -1,8 +0,0 @@
-export const up = (knex) => knex.schema.createTable('blacklist', (table) => {
-  table.increments();
-  table.string('timer_id');
-  table.string('reason');
-  table.timestamp('created_at').defaultTo(knex.fn.now());
-});
-
-export const down = (knex) => knex.schema.dropTable('blacklist');
