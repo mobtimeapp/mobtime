3b70295754bdac23d21e0be7d948bae45480276a
Alex Barry <alex.barry@gmail.com>
2020-08-14 16:44:04 -0400

    TDD Update logic

--- src/update.js
+++ src/update.js
@@ -1,40 +1,60 @@
-import {effects} from 'ferp';
+import { effects } from 'ferp';
 import Action from './actions';
 
 export const update = (action, state) => {
-    if (!action) return [state, effects.none()];
+  if (!action) return [state, effects.none()];
 
-    return Action.caseOf({
-        Init: () => [
-            {
-                connections: [],
-            },
-            effects.none(),
-        ],
+  return Action.caseOf({
+    Init: () => [
+      {
+        connections: [],
+      },
+      effects.none(),
+    ],
 
-        AddConnection: (websocket, timerId, isOwner) => {
-            return [
-                {
-                    ...state,
-                    connections: state.connections.concat({
-                        websocket,
-                        timerId,
-                        isOwner
-                    })
-                },
-                effects.none(),
-            ]
+    AddConnection: (websocket, timerId) => {
+      const isOwner = state.connections.every((c) => c.timerId !== timerId);
+      return [
+        {
+          ...state,
+          connections: state.connections.concat({
+            websocket,
+            timerId,
+            isOwner,
+          }),
         },
+        effects.none(),
+      ];
+    },
 
-        RemoveConnection: (websocket) => {
-            return [
-                {
-                    ...state,
-                    connections: state.connections.filter(connection => websocket !== connection.websocket)
-                }
-            ]
+    RemoveConnection: (websocket) => [
+      {
+        ...state,
+        connections: state.connections.filter((connection) => (
+          websocket !== connection.websocket
+        )),
+      },
+      effects.none(),
+    ],
+
+    MessageTimer: (websocket, timerId, message) => {
+      const websockets = state.connections.reduce((sockets, connection) => {
+        const differentTimer = connection.timerId !== timerId;
+        const isOriginatingWebsocket = connection.websocket === websocket;
+        if (differentTimer || isOriginatingWebsocket) {
+          return sockets;
         }
 
-    }, action);
+        return [...sockets, connection.websocket];
+      }, []);
 
+      return [
+        state,
+        effects.thunk(() => {
+          websockets.forEach((ws) => ws.send(message));
+          return effects.none();
+        }),
+      ];
+    },
+  }, action);
 };
