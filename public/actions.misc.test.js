c91fe5add2d6f9500a4827c4fba82d5c04c99a86
Alex Barry <mrozbarry@users.noreply.github.com>
2020-08-17 16:57:05 -0400

    Add tests to client side of the app (#116)
    * WIP More test coverage
    
    * Fix test using wrong action and only sometimes? failing
    
    * Send information to clients if they are the owner or not
    
    * So much more test coverage

---
+++ public/actions.misc.test.js
@@ -0,0 +1,34 @@
+import test from 'ava';
+
+import * as actions from './actions';
+import * as effects from './effects';
+
+test('can set websocket', (t) => {
+  const websocket = { send: () => {} };
+  const state = actions.SetWebsocket({}, { websocket });
+  t.deepEqual(state, { websocket });
+});
+
+test('can set expand reorderable', (t) => {
+  const expandedReorderable = { foo: 'bar' };
+  const state = actions.ExpandReorderable({}, { expandedReorderable });
+  t.deepEqual(state, { expandedReorderable });
+});
+
+test('can set timer tab', (t) => {
+  const timerTab = 'foo-bar-baz';
+  const state = actions.SetTimerTab({}, timerTab);
+  t.deepEqual(state, { timerTab });
+});
+
+test('can set remaining timer', (t) => {
+  const remainingTime = 12345;
+  const [state, effect] = actions.SetRemainingTime({}, remainingTime);
+  t.deepEqual(state, { remainingTime });
+  t.deepEqual(effect, effects.UpdateTitleWithTime({ remainingTime }));
+});
+
+test('can set allow notification', (t) => {
+  const state = actions.SetAllowNotification({}, true);
+  t.deepEqual(state, { allowNotification: true });
+});
