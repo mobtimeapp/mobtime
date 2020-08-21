9bc7a06944190ce9f1cf751e84f056a6ef7a272b
Alex Barry <alex.barry@gmail.com>
2020-08-21 08:15:21 -0400

    Extract calculateTimeRemaining from timeRemaining component
     - Also some cleanup in actions

---
+++ public/lib/calculateTimeRemaining.js
@@ -0,0 +1,10 @@
+export const calculateTimeRemaining = (props) => {
+  if (!props.timerStartedAt) {
+    return props.timerDuration;
+  }
+
+  const elapsed = props.currentTime - props.timerStartedAt;
+  return props.timerDuration > 0
+    ? Math.max(0, props.timerDuration - elapsed)
+    : 0;
+};
