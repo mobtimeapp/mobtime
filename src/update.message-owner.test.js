a263b2c536a2e09637fc0da6ab0b369094bc1383
Alex Barry <alex.barry@gmail.com>
2020-08-22 15:49:48 -0400

    Full test coverage of all the server update code

--- src/update.message-owner.test.js
+++ src/update.message-owner.test.js
@@ -1,38 +1,34 @@
 import test from 'ava';
 import { effects } from 'ferp';
-import { id } from './id';
 import sinon from 'sinon';
 
 import { update } from './update';
 import Action from './actions';
 
-const makeState = (actions) => {
-  const [state] = actions.reduce(([state], action) => (
-    update(action, state)
-  ), update(Action.Init(), {}));
-  return state;
-};
+import * as Connection from './connection';
 
 test('sends a message just to the timer owner', (t) => {
   const timerId = 'foo';
   const ownerWebsocket = { send: sinon.fake() };
   const otherWebsocket = { send: sinon.fake() };
-  const initialState = makeState([
-    Action.AddConnection(ownerWebsocket, timerId),
-    Action.AddConnection(otherWebsocket, timerId),
-  ]);
+  const initialState = {
+    connections: [
+      Connection.make(ownerWebsocket, timerId, true),
+      Connection.make(otherWebsocket, timerId, false),
+    ],
+  };
 
   const message = 'test';
 
-  const [state, fx] = update(
+  const [state, effect] = update(
     Action.MessageTimerOwner({}, timerId, message),
     initialState,
   );
 
   t.is(state, initialState);
-  t.is(fx.type, effects.thunk(() => {}).type);
+  t.is(effect.type, effects.thunk(() => {}).type);
 
-  fx.method();
+  effect.method();
 
   t.truthy(ownerWebsocket.send.calledOnceWithExactly(message));
   t.falsy(otherWebsocket.send.calledOnceWithExactly(message));
@@ -42,22 +38,25 @@ test('does not send a message just to the timer owner if the originator is the o
   const timerId = 'foo';
   const ownerWebsocket = { send: sinon.fake() };
   const otherWebsocket = { send: sinon.fake() };
-  const initialState = makeState([
-    Action.AddConnection(ownerWebsocket, timerId),
-    Action.AddConnection(otherWebsocket, timerId),
-  ]);
+
+  const initialState = {
+    connections: [
+      Connection.make(ownerWebsocket, timerId, true),
+      Connection.make(otherWebsocket, timerId, false),
+    ],
+  };
 
   const message = 'test';
 
-  const [state, fx] = update(
+  const [state, effect] = update(
     Action.MessageTimerOwner(ownerWebsocket, timerId, message),
     initialState,
   );
 
   t.is(state, initialState);
-  t.is(fx.type, effects.thunk(() => {}).type);
+  t.is(effect.type, effects.thunk(() => {}).type);
 
-  fx.method();
+  effect.method();
 
   t.falsy(ownerWebsocket.send.calledOnceWithExactly(message));
   t.falsy(otherWebsocket.send.calledOnceWithExactly(message));
