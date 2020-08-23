b57b84ddb0187f49228ef1d5f3013a4c4af8042b
Alex Barry <alex.barry@gmail.com>
2020-08-23 17:08:40 -0400

    Increase public test coverage

--- public/api.js
+++
@@ -1,9 +0,0 @@
-export default (endpoint, token, options = {}) => {
-  const headers = {
-    ...options.headers,
-    Accept: 'application/json',
-    Authorization: `token ${token}`,
-  };
-
-  return fetch(endpoint, { ...options, headers });
-};
