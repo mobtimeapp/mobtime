6bad3d3d58e4209944841b4ae9f68ca1f7d6a0be
Alex Barry <alex.barry@gmail.com>
2020-08-25 20:36:23 -0400

    Add UI for multiple goals on separate lines

--- public/components/input.js
+++ public/components/input.js
@@ -9,3 +9,13 @@ export const input = (props) => h('input', {
     ...(props.class || {}),
   },
 });
+
+export const textarea = (props, children) => h('textarea', {
+  ...props,
+  class: {
+    'border-b-4': true,
+    'border-b-solid': true,
+    'px-2': true,
+    ...(props.class || {}),
+  },
+}, children);
