4cd4177c824f123c0efee4bb2bd2091930e13c81
frankyfrankfrank <adam@webadamfrank.com>
2020-10-02 17:22:59 -0400

    install and generate tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [
+    './public/**/*.html',
+    './public/**/*.js',
+    './src/**/*.html',
+    './src/**/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
