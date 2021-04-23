7b0c5ee9062f26a57dddcac49b18eb04cdf5098b
Alex Barry <alex.barry@gmail.com>
2021-04-23 16:40:09 -0400

    Filled out more edit timer form stuff
     - Also made buttons and modals way more consistent

--- public/components/button.js
+++ public/components/button.js
@@ -1,10 +1,38 @@
 import { h } from '/vendor/hyperapp.js';
 
 const sizes = {
-  sm: { 'px-1': true },
-  md: { 'py-1': true, 'px-2': true },
-  lg: { 'py-2': true, 'px-4': true },
+  sm: 'px-1',
+  md: 'py-1 px-2',
+  lg: 'py-2 px-4',
 };
+
+const classToObject = myClass => {
+  if (typeof myClass === 'string') return classToObject(myClass.split(' '));
+  if (Array.isArray(myClass)) {
+    return myClass
+      .filter(str => str)
+      .reduce((obj, str) => ({ ...obj, [str]: true }), {});
+  }
+  return myClass;
+};
+
+const combineClass = (...classes) =>
+  classes
+    .filter(c => c)
+    .reduce(
+      (memo, currentClass) => ({
+        ...memo,
+        ...classToObject(currentClass),
+      }),
+      {},
+    );
+
+const buttonColorClasses = bgColorBase => [
+  `bg-${bgColorBase}-200 text-gray-900`,
+  `hover:bg-${bgColorBase}-300`,
+  `dark:bg-${bgColorBase}-800 dark:text-gray-200`,
+  `dark:hover:bg-${bgColorBase}-700`,
+];
 
 export const button = (props = {}, children) =>
   h(
@@ -12,13 +40,11 @@ export const button = (props = {}, children) =>
     {
       type: 'button',
       ...props,
-      class: {
-        ...(sizes[props.size] || sizes.lg),
-        'tracking-widest': true,
-        "uppercase": true,
-        "shadow": 'shadow' in props ? props.shadow : true,
-        ...(props.class || {}),
-      },
+      class: combineClass(
+        sizes[props.size] || sizes.lg,
+        props.class,
+        buttonColorClasses(props.color || 'gray'),
+      ),
     },
     [].concat(children),
   );
--- public/components/modal.js
+++ public/components/modal.js
@@ -8,7 +8,7 @@ export const modal = children =>
       'div',
       {
         class: [
-          'p-2 mt-16 mx-2',
+          'px-4 pb-2 mt-16 mx-2',
           'w-full sm:w-3/4 sm:mx-0',
           'bg-white text-gray-900',
           'dark:bg-gray-900 dark:text-gray-200',
