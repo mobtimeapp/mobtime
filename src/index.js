7c0f4d39dbc9e7a093c60a8e27caf3515cdb450a
Mpho Mphego <mpho112@gmail.com>
2021-05-07 15:45:50 +0200

    Containerized the application

--- src/index.js
+++ src/index.js
@@ -5,14 +5,14 @@ import { Http } from './http';
 import { Websocket } from './websocket';
 
 const port = process.env.PORT || 1234;
-
+const host = process.env.HOST || 'localhost';
 const Storage = storage.make();
 
 app({
   init: Action.Init(),
 
   subscribe: state => [
-    Http(Storage, Action, 'localhost', port),
+    Http(Storage, Action, host, port),
     ...state.connections.map((connection) => (
       Websocket(Action, connection, connection.timerId)
     )),
