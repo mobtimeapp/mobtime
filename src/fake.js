31b4b169ae74bae0cc47d7bd74344016d17a1048
Alex Barry <alex.barry@gmail.com>
2021-08-03 22:27:22 -0400

    Failed attempt at node <=> redis connection

--- src/fake.js
+++ src/fake.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import { networkInterfaces } from 'os';
+import * as redis from 'redis';
 
 const port = Number(process.env.PORT) || 4321;
+const redisUrl = process.env.REDIS_URL;
 
 const app = express();
 
@@ -10,6 +12,18 @@ const ip = []
   .find(x => !x.internal && x.family === 'IPv4');
 
 const address = ip ? ip.address : 'unknown';
+
+const pub = redis.createClient(redisUrl);
+// const sub = redis.createClient({ host: 'redis' });
+
+// sub.on('subscribe', (channel, count) => {
+// console.log('redis.subscribe', address, channel, count);
+// pub.publish('mobtime', address);
+// });
+
+// sub.on('message', (channel, message) => {
+// console.log('redis.message', channel, message);
+// });
 
 app.use((_request, response) => {
   response
@@ -21,3 +35,11 @@ app.use((_request, response) => {
 app.listen(port, () => {
   console.log(`Server ready http://${address}:${port}`); // eslint-disable-line no-console
 });
+
+process.on('exit', () => {
+  // sub.unsubscribe();
+  pub.quit();
+  // sub.quit();
+});
+
+// sub.subscribe('mobtime');
