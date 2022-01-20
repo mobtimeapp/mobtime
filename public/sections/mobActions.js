79fa27181001658d78bb8516636f70af88a4fdd8
Alex Barry <alex.barry@gmail.com>
2022-01-19 21:47:47 -0500

    Make the mob action buttons look more like buttons

--- public/sections/mobActions.js
+++ public/sections/mobActions.js
@@ -1,4 +1,4 @@
-import { text } from '/vendor/hyperapp.js';
+import { text, h } from '/vendor/hyperapp.js';
 
 import { section } from '/components/section.js';
 import { button } from '/components/button.js';
@@ -19,25 +19,30 @@ export const mobActions = () =>
       button(
         {
           class: {
-            'text-indigo-600': true,
+            'bg-green-600': true,
+            'text-white': true,
             'flex-grow': true,
             'mr-1': true,
           },
           onclick: actions.CycleMob,
         },
-        text('Rotate'),
+        [h('x-icon', { class: 'inline fas fa-sync-alt mr-1' }), text('Rotate')],
       ),
 
       button(
         {
           class: {
-            'text-indigo-600': true,
+            'bg-green-600': true,
+            'text-white': true,
             'flex-grow': true,
             'ml-1': true,
           },
           onclick: actions.ShuffleMob,
         },
-        text('Randomize'),
+        [
+          h('x-icon', { class: 'inline fas fa-random mr-1' }),
+          text('Randomize'),
+        ],
       ),
     ],
   );
