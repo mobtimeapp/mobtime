ffef04b7d0e3acabfc1a7095a126b2fcbae136d9
Alex Barry <alex.barry@gmail.com>
2022-01-31 09:27:04 -0500

    only use clustering intentionally

---
+++ src/server.js
@@ -0,0 +1,38 @@
+import { app } from 'ferp';
+import * as Action from './actions';
+import { Http } from './http';
+import { Websocket } from './websocket';
+import { Queue } from './queue';
+import { cpus } from 'os';
+import process from 'process';
+
+const port = process.env.APP_PORT || 1234;
+const host = process.env.APP_HOST || 'localhost';
+
+export const run = () => {
+  app({
+    init: Action.Init(new Queue()),
+
+    subscribe: state => {
+      const timerIds = Object.keys(state.connections).sort();
+
+      return [
+        Http(Action, host, port),
+
+        ...timerIds.reduce(
+          (subs, timerId) => [
+            ...subs,
+            ...state.connections[timerId].map(connection =>
+              Websocket(Action, connection, connection.timerId),
+            ),
+          ],
+          [],
+        ),
+
+        ...timerIds.map(timerId =>
+          state.queue.subscribeToTimer(timerId, Action.MessageTimer),
+        ),
+      ];
+    },
+  });
+};
