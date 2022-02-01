a39c15f22cebd64a7a94048a0f367c50dbb89e5f
Alex Barry <alex.barry@gmail.com>
2022-02-01 09:20:44 -0500

    Intelligentally decide to use clustering based on ENV

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
+require('dotenv/config');
 require = require("esm")(module/*, options*/)
-module.exports = require("./src/index.js")
+if (process.env.WORKERS > 1 || process.env.WORKERS === -1) {
+  module.exports = require("./src/index.cluster.js")
+} else {
+  module.exports = require("./src/index.js")
+}
