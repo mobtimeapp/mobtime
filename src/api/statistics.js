e96a389477479fe5fca40c579697f313581f1eeb
Alex Barry <alex.barry@gmail.com>
2022-02-02 21:14:23 -0500

    Cache the timer statistics

--- src/api/statistics.js
+++ src/api/statistics.js
@@ -1,30 +1,41 @@
 import express from 'express';
 import { Queue } from '../queue';
 
+const CACHE_TTL = 1000 * 60;
+
 export default () => {
   const router = new express.Router();
   const queue = new Queue();
+  let cache = {
+    value: { connections: 0, mobbers: 0, timerIds: 0 },
+    time: Date.now() - CACHE_TTL - 1,
+  };
 
   router.get('/statistics', async (_request, response) => {
-    const stats = await queue.getStatistics();
-    const timerIds = Object.keys(stats || {});
-    const timerStatistics = timerIds.reduce(
-      (counts, id) => ({
-        connections: counts.connections + stats[id].connections,
-        mobbers:
-          stats[id].connections > 0
-            ? counts.mobbers + stats[id].mobbers
-            : counts.mobbers,
-      }),
-      { connections: 0, mobbers: 0 },
-    );
+    let timerStatistics = cache.value;
+    if (Date.now() - cache.time > CACHE_TTL) {
+      const stats = await queue.getStatistics();
+      timerIds = Object.keys(stats || {});
+      timerStatistics = timerIds.reduce(
+        (counts, id) => ({
+          connections: counts.connections + stats[id].connections,
+          mobbers:
+            stats[id].connections > 0
+              ? counts.mobbers + stats[id].mobbers
+              : counts.mobbers,
+        }),
+        { connections: 0, mobbers: 0 },
+      );
+      timerStatistics.timerIds = timerIds.length;
+      cache = {
+        value: timerStatistics,
+        time: Date.now(),
+      };
+    }
 
     return response
       .status(200)
-      .json({
-        timers: timerIds.length,
-        ...timerStatistics,
-      })
+      .json(timerStatistics)
       .end();
   });
 
