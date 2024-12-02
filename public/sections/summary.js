f2a8a2d6f3eb2faf4ead12448e66e30d066e6c2a
Alex Barry <alex.barry@gmail.com>
2024-12-02 16:30:20 -0500

    Make mob positions and timer actions easier to find on the page

--- public/sections/summary.js
+++ public/sections/summary.js
@@ -1,6 +1,6 @@
-import { h, text } from "/vendor/hyperapp.js";
+import { h, text } from '/vendor/hyperapp.js';
 
-import * as actions from "/actions.js";
+import * as actions from '/actions.js';
 
 const subGoalRegex = () => /^[- \t]+/;
 
@@ -32,78 +32,81 @@ export const summary = (props) => {
     }
   }
 
-  return h("div", {
-    //class: "flex items-center justify-start",
+  return h('div', {
+    // class: "flex items-center justify-start",
   }, [
-    h("ol", {}, [
-      ...props.settings.mobOrder.split(",").map((position, index) => {
-        return h("li", { class: "mb-2" }, [
+    h('ol', {}, [
+      ...props.settings.mobOrder.split(',').map((position, index) => {
+        return h('li', { class: 'mb-2', 'data-mobposition': position }, [
           h(
-            "div",
-            { class: "text-xs leading-none uppercase text-slate-600" },
+            'div',
+            { class: 'text-xs leading-none uppercase text-slate-600' },
             text(position),
           ),
-          h("div", {}, text(props.mob[index]?.name || "Empty")),
+          h('div', {}, text(props.mob[index]?.name || 'Empty')),
         ]);
       }),
     ]),
-    incomplete.goal && h("div", { class: "mb-2" }, [
-      h("hr", { class: "mb-2" }),
+    incomplete.goal && h('div', { class: 'mb-2' }, [
+      h('hr', { class: 'mb-2' }),
       h(
-        "div",
-        { class: "text-xs leading-none uppercase text-slate-600" },
-        text("Current Goal"),
+        'div',
+        { class: 'text-xs leading-none uppercase text-slate-600' },
+        text('Current Goal'),
       ),
-      h("div", { class: "" }, [
+      h('div', { class: '' }, [
         text(incomplete.goal.text),
         incomplete.subGoal &&
         h(
-          "div",
-          { class: "ml-2" },
-          text(incomplete.subGoal.text.replace(subGoalRegex(), "")),
+          'div',
+          { class: 'ml-2' },
+          text(incomplete.subGoal.text.replace(subGoalRegex(), '')),
         ),
       ]),
     ]),
-    h("div", {}, [
-      h("hr", { class: "mb-2" }),
+    h('div', {}, [
+      h('hr', { class: 'mb-2' }),
       h(
-        "div",
-        { class: "text-xs uppercase text-slate-600" },
-        text("Turn Controls"),
+        'div',
+        { class: 'text-xs uppercase text-slate-600' },
+        text('Turn Controls'),
       ),
-      h("div", { class: "grid grid-cols-3" }, [
-        h("button", {
-          type: "button",
+      h('div', { class: 'grid grid-cols-3' }, [
+        h('button', {
+          'data-timercontrol': 'start',
+          type: 'button',
           class: [
-            "mr-1 px-2 py-1 border border-slate-700",
+            'mr-1 px-2 py-1 border border-slate-700',
             props.timerStartedAt &&
-            "bg-slate-200 text-black cursor-not-allowed",
-          ].filter(Boolean).join(" "),
+            'bg-slate-200 text-black cursor-not-allowed',
+          ].filter(Boolean).join(' '),
           disabled: props.timerStartedAt,
           onclick: props.timerDuration
             ? [actions.ResumeTimer, {}]
             : [actions.StartTimer, {
-              timerStartedAt: Date.now(),
-              timerDuration: props.settings.duration,
-            }],
-        }, text("Start")),
-        h("button", {
-          type: "button",
+                timerStartedAt: Date.now(),
+                timerDuration: props.settings.duration,
+              }],
+        }, text('Start')),
+        h('button', {
+          'data-timercontrol': 'pause',
+          type: 'button',
           class: [
-            "mr-1 px-2 py-1 border border-slate-700",
+            'mr-1 px-2 py-1 border border-slate-700',
             !props.timerStartedAt &&
-            "bg-slate-200 text-black cursor-not-allowed",
-          ].filter(Boolean).join(" "),
+            'bg-slate-200 text-black cursor-not-allowed',
+          ].filter(Boolean).join(' '),
           disabled: !props.timerStartedAt,
           onclick: [actions.PauseTimer, undefined],
-        }, text("Pause")),
-        h("button", {
-          type: "button",
+        }, text('Pause')),
+        h('button', {
+          'data-timercontrol': 'skip',
+          type: 'button',
           class: [
-            "mr-1 px-2 py-1 border border-slate-700",
+            'mr-1 px-2 py-1 border border-slate-700',
             !props.timerDuration &&
-            "bg-slate-200 text-black cursor-not-allowed",
-          ].join(" "),
+            'bg-slate-200 text-black cursor-not-allowed',
+          ].join(' '),
           disabled: !props.timerDuration,
           onclick: [
             actions.Completed,
@@ -113,7 +116,7 @@ export const summary = (props) => {
               Notification: window.Notification,
             },
           ],
-        }, text("Skip")),
+        }, text('Skip')),
       ]),
     ]),
   ]);
